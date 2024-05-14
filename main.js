290569225840a6f7655ed01c198200194c99b848
Alireza Khodadadi <alirezakh0199@gmail.com>
2024-05-14 10:38:09 +0330

    Fix bug

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 // !! Dom Elements
 const app = document.querySelector('#app');
 // !! View
-import * as CourselProductElem from './view/coursel';
+import * as CourselProductElem from './view/carousel';
 import * as ProductElemDevice from './view/product';
 
 app.innerHTML = `
